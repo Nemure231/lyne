a4c37bba7371636c09b58a2f5d96367f71b8643f
Nemure231 <neruhi5321@gmail.com>
2022-05-28 17:11:43 +0700

    add request to api

---
+++ components/Base/Body.vue
@@ -0,0 +1,27 @@
+
+<script setup lang="ts">
+
+interface StickerData {
+    success?: boolean;
+    message?: string;
+    data?: object;
+}
+
+const stickers = ref<StickerData>({
+    success: false,
+    message: '',
+    data: {}
+});
+
+const fec = async (id: number, reg: string) => {
+    const response = await $fetch(`/api/sticker/${id}/${reg}`);
+    stickers.value = response
+}
+
+</script>
+<template>
+    <div>
+        {{stickers.message}}
+        <button @click="fec(4857276, 'en')">Tekan</button>
+    </div>
+</template>
---
+++ components/Base/Nav.vue
@@ -0,0 +1,10 @@
+
+<template>
+    <!-- <nav font-sans z-50 top-0 fixed bg-green-400 h-12 w-full>
+        <div flex flex-row gap-x-6 flex-wrap text-lg items-center px-12 font-semibold h-full>
+            <NuxtLink  to="/" no-underline text-black>Home</NuxtLink>
+            <NuxtLink  to="/generator" no-underline text-black>Generator</NuxtLink>
+            <NuxtLink  to="/resto" no-underline text-black>Resto</NuxtLink>
+        </div>
+    </nav> -->
+</template>
