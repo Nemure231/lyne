d9cfa50af471421e2fc2af5d68174d631361acb4
Nemure231 <neruhi5321@gmail.com>
2022-05-31 22:10:17 +0700

    fix github link

--- components/Base/Footer.vue
+++ components/Base/Footer.vue
@@ -2,6 +2,6 @@
     <footer fixed bottom-3 right-3 z-10 font-sans>
         <div text-base text-black dark:text-light> Via 
             
-            <a font-bold text-green-500 href="https://github.com/Nemure231/lineline">Github</a></div>
+            <a font-bold text-green-500 href="https://github.com/Nemure231/lyne">Github</a></div>
     </footer>
 </template>
