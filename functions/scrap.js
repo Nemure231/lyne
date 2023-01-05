8ac34f095e12ee81fbaf3499d65d5cfc9e808c5c
Nemure231 <neruhi5321@gmail.com>
2023-01-06 02:30:20 +0700

    chore: redeploy

--- functions/scrap.js
+++ functions/scrap.js
@@ -1,5 +1,5 @@
 const chromium = require('chrome-aws-lambda')
-const puppeteer = require('puppeteer-core')
+const puppeteer = require('puppeteer')
 
 exports.handler = async (event, context, callback) => {
   let theTitle = null
